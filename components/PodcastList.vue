ba5bc875529b7a5b5a3b9f5649c8142d84f4f810
Manuel Zambrano <dev.mzambrano@gmail.com>
2020-11-14 12:52:31 +0100

    feat(podcast): add podcast link and page

---
+++ components/PodcastList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <article
+      v-for="item in items"
+      :key="item.guid"
+      class="flex flex-col justify-between items-center rounded shadow-lg p-5 my-5 hover:shadow-xl dark:bg-gray-900"
+    >
+      <div class="flex flex-col sm:flex-row justify-between items-center">
+        <div class="sm:mb-0 mb-4">
+          <img :src="item.itunes.image" class="w-40" />
+        </div>
+        <div class="ml-4">
+          <h3 class="font-bold text-xl">{{ item.title }}</h3>
+          <p>{{ item.contentSnippet }}</p>
+        </div>
+      </div>
+      <div class="flex w-full mt-2">
+        <audio controls class="flex w-full rounded">
+          <source :src="item.enclosure.url" :type="item.enclosure.type" />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ pages/podcast/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="mt-10 flex-1 w-full h-full py-2">
+    <section class="">
+      <h2 class="text-2xl">Todos los capítulos del podcast</h2>
+      <PodcastList :items="items" />
+    </section>
+  </div>
+</template>
+
+<script>
+const Parser = require('rss-parser')
+
+export default {
+  async asyncData() {
+    const rss = await new Parser().parseURL(
+      'https://anchor.fm/s/3f0ee6d0/podcast/rss'
+    )
+    const { items } = rss
+
+    return { items }
+  },
+}
+</script>
